7b43817bd161f5be80b2a02d6c7aa6e6e032952b
Senpai-OwO <59488227+Senpai-OwO@users.noreply.github.com>
2020-01-03 17:34:33 -0500

    Update fact.js

--- load/fact.js
+++ load/fact.js
@@ -1,3 +1,3 @@
-var facts = ["Your facts", "...", "..."];
-var fact = Math.floor(Math.random() * facts.length);
-message.channel.send(facts[fact]);
+if(message.content.includes('ping')) {
+    message.reply('Pong');
+}
