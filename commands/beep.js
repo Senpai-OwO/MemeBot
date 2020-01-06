4f37bcb5453cd9b33c8b7192ff018117ec451d2d
Senpai-OwO <59488227+Senpai-OwO@users.noreply.github.com>
2020-01-06 00:11:19 -0500

    Update beep.js

--- commands/beep.js
+++ commands/beep.js
@@ -3,6 +3,6 @@ module.exports = {
 	aliases: ['bop'],
 	desc: 'Makes a little robot sound.',
 	exec: (call) => {
-		call.message.channel.send('Bop! I am a meme bot!');
+		call.message.channel.send('Booooop! Itsa me the Cereal Senpai Bot');
 	}
 };
