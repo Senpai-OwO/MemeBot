bc074da37278304e1dd0624d817b3d2f3be05073
Senpai-OwO <59488227+Senpai-OwO@users.noreply.github.com>
2020-12-14 15:04:09 -0500

    Update dog.js

--- commands/dog.js
+++ commands/dog.js
@@ -3,6 +3,6 @@ module.exports = {
 	aliases: ['doggie'],
 	desc: 'Sends a random picture of a dog.',
 	exec: (call) => {
-		call.message.channel.send("https://cdn.shopify.com/s/files/1/0198/9715/5646/products/white-wh_5000x.png?v=1602855688");
+		call.message.channel.send('https://cdn.shopify.com/s/files/1/0198/9715/5646/products/white-wh_5000x.png?v=1602855688');
 	}
 };
