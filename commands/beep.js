d9b0ed48ce81e5adfcd63ca250f32340b2b11bc3
Senpai-OwO <59488227+Senpai-OwO@users.noreply.github.com>
2020-12-13 21:13:41 -0500

    Update beep.js

--- commands/beep.js
+++ commands/beep.js
@@ -3,6 +3,6 @@ module.exports = {
 	aliases: ['bop'],
 	desc: 'Makes a little robot sound.',
 	exec: (call) => {
-		call.message.channel.send('Booooop!', 'Beep!', 'Leave me alone.');
+		call.message.channel.send('Booooop!'), ('Beep!');
 	}
 };
