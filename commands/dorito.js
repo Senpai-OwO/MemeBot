7738bd4c866320c7b35b513f9c01896d06415156
Senpai-OwO <59488227+Senpai-OwO@users.noreply.github.com>
2020-12-14 17:42:42 -0500

    Create dorito.js

---
+++ commands/dorito.js
@@ -0,0 +1,6 @@
+module.exports = {
+	id: 'dorito',
+	exec: (call) => {
+		call.message.channel.send('https://www.redbubble.com/i/sticker/Nacho-Cheese-Dorito-Painting-by-DesignDLW/43130948.EJUG5');
+	}
+};
