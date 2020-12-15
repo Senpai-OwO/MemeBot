638dbeef52935d249ca73fe6ab6112c8fc90bb74
Senpai-OwO <59488227+Senpai-OwO@users.noreply.github.com>
2020-12-15 11:05:04 -0500

    Update dog.js

--- commands/dog.js
+++ commands/dog.js
@@ -1,21 +1,18 @@
-var textArray = [
-    new Image().src = 'https://cdn.discordapp.com/attachments/771827343072297024/788161574664929340/image0.jpg',
-    new Image().src = 'https://cdn.shopify.com/s/files/1/0198/9715/5646/products/white-wh_5000x.png?v=1602855688',
-    new Image().src = 'https://cdn.discordapp.com/attachments/788162087142424597/788162138983104543/Screenshot_20201214-042907_Instagram.jpg',
+var Discord = require('discord.js');
+var bot = new Discord.Client();
 
-];
+bot.on('message', message => {
+    var prefix = ','
+    var msg = message.content;
 
-module.exports = {
-    id: 'dog',
-    aliases: ['doggie'],
-    desc: 'Random picture of a dog',
-    getRandomImageURI: (callback) => {
-
-        var randomIndex = Math.floor(Math.random() * textArray.length)
-
-        callback(textArray[randomIndex])
-
-        return textArray[randomIndex]
-
+    if (msg === prefix + 'image') {
+        message.channel.send('Message that goes above image', {
+            files: [
+                "https://cdn.discordapp.com/attachments/771827343072297024/788161574664929340/image0.jpg"
+            ]
+        });
     }
-}
+});
+
+bot.login('TOKEN');
+
