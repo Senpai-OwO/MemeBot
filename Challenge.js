a15b76e75f3edb42da06bfac1cd0f7f31742b65f
Senpai-OwO <59488227+Senpai-OwO@users.noreply.github.com>
2020-12-15 11:18:55 -0500

    Delete Challenge.js

--- Challenge.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	id: 'challenge',
-	aliases: ['tic tac toe'],
-	desc: 'Challenge the bot to tic tac toe!',
-	exec: (call) => {
-		call.message.channel.send('This command is currently disabled.');
-	}
-};
