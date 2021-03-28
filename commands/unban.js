f5b517129c5dc4d694ddbbb47d910a97db60282c
Senpai-OwO <59488227+Senpai-OwO@users.noreply.github.com>
2021-03-27 22:59:30 -0400

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -1,16 +1,18 @@
-module.exports = {
-	id: 'invban',
-	aliases: ['unban'],
-	desc: 'Unbans a user.',
-	exec: (call) => {
-                    bot.on('message', message => {
-    if(message.content == ',invban'){
-        message.guild.fetchBans().then(bans => {
-            bans.forEach(user => {
-                console.log(user.username + '#' + user.tag);
-                user.send('MESSAGE / INVITE LINK');
-                message.guild.unban(user);
-            });
-        });
+if(command === "unban"){
+    if(!msg.member.hasPermission("BAN_MEMBERS")) {
+      return msg.channel.send(`**${msg.author.username}**, You do not have perms to unban someone`)
     }
-});
+    
+    if(!msg.guild.me.hasPermission("BAN_MEMBERS")) {
+      return msg.channel.send(`**${msg.author.username}**, I do not have perms to unban someone`)
+    }
+    
+    let userID = args[0]
+      msg.guild.fetchBans().then(bans=> {
+      if(bans.size == 0) return 
+      let bUser = bans.find(b => b.user.id == userID)
+      if(!bUser) return
+      msg.guild.members.unban(bUser.user)
+})
+    
+  ;}
